135c4a9bfbb3d563f551d248fefffbf221fc0c3c
Manoj Kumar <mk.guggilam@gmail.com>
2018-03-01 05:43:55 +0530

    Adding pilot prog

---
+++ Calculator.java
@@ -0,0 +1,33 @@
+
+public class Calculator {
+
+	public int add(int x, int y) {
+		int j = x+y;
+		System.out.println(j);
+		return j;
+	}
+	
+	public int sub(int x, int y) {
+		
+		return x-y;
+	}
+	
+	public int mul(int x, int y) {
+		
+		return x*y;
+	}
+	
+	public int div(int x, int y) {
+		
+		return x/y;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator c = new Calculator();
+		c.add(4, 8);
+		int j = c.sub(10, 5);
+		System.out.println(j);
+	}
+
+}
